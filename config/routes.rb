ad0138cf6d8043ae940106ce1bf38e75b591f183
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-23 17:44:09 -0700

    created survey and response controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'responses/create'
+  get 'surveys/index'
+  get 'surveys/new'
+  get 'surveys/create'
+  get 'surveys/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
