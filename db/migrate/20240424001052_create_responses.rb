2be897ef8e6bce9e36a90d979ac768fd3b262384
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-23 17:42:31 -0700

    removing the explicit declaration of creaed_at

--- db/migrate/20240424001052_create_responses.rb
+++ db/migrate/20240424001052_create_responses.rb
@@ -3,7 +3,6 @@ class CreateResponses < ActiveRecord::Migration[7.1]
     create_table :responses do |t|
       t.references :survey, null: false, foreign_key: true
       t.boolean :answer
-      t.datetime :created_at
 
       t.timestamps
     end
