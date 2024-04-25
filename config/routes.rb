557fcb8a72ce5b7defc1b9eda38e9a4954d8683a
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-24 18:39:20 -0700

    add route setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'responses/create'
-  get 'surveys/index'
-  get 'surveys/new'
-  get 'surveys/create'
-  get 'surveys/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'surveys#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :surveys, only: [:index, :new, :create, :show] do
+    resources :responses, only: [:create]
+  end
 end
