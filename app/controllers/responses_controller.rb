5abe5b0251c9b55b807976e88fc5e14b76cbb929
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-24 18:40:44 -0700

    corrected controller actions

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,4 +1,18 @@
 class ResponsesController < ApplicationController
   def create
+    @survey = Survey.find(params[:survey_id])
+    @response = @survey.responses.new(response_params)
+
+    if @response.save
+      redirect_to @survey, notice: 'Your response has been recorded.'
+    else
+      render 'surveys/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:answer)
   end
 end
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,13 +1,30 @@
 class SurveysController < ApplicationController
   def index
+    @surveys = Survey.includes(:responses).all
   end
 
   def new
-  end
-
-  def create
+    @survey = Survey.new
   end
 
   def show
+    @survey = Survey.find(params[:id])
+    @response = @survey.responses.new
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      redirect_to surveys_path, notice: 'Survey was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:question)
   end
 end
