0f94b8cd4824c30855703096dd8776b14f6d5977
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-26 11:03:30 -0700

    eliminate n+1 query

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,9 @@
 class SurveysController < ApplicationController
   def index
     @surveys = Survey.includes(:responses).order(created_at: :desc)
+                     .select('surveys.*, COUNT(CASE WHEN responses.answer = true THEN 1 END) AS yes_count, COUNT(CASE WHEN responses.answer = false THEN 1 END) AS no_count')
+                     .left_joins(:responses)
+                     .group('surveys.id')
   end
 
   def new
@@ -8,9 +11,11 @@ class SurveysController < ApplicationController
   end
 
   def show
-    @survey = Survey.find(params[:id])
+    @survey = Survey.find_by(id: params[:id])
     if @survey
       @response = @survey.responses.new
+      @yes_count = @survey.responses.where(answer: true).count
+      @no_count = @survey.responses.where(answer: false).count
     else
       redirect_to surveys_path, alert: 'Survey not found.'
     end
