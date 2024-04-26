9fbe55b5a38a9143fad023b8a5ceab7f0f2863ef
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-26 09:49:11 -0700

    added validation for max 150 characters

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,4 @@
 class Survey < ApplicationRecord
   has_many :responses
+  validates :question, presence: true, length: { maximum: 150 }
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Survey, type: :model do
   describe 'associations' do
     it { should have_many(:responses) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:question) }
+    it { should validate_length_of(:question).is_at_most(150) }
+  end
 end
