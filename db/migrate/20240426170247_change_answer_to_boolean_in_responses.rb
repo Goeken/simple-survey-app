85934cd4899ec4b9c0abfc336dab13b2c741d488
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-26 10:47:03 -0700

    removed default for answer

--- db/migrate/20240426170247_change_answer_to_boolean_in_responses.rb
+++ db/migrate/20240426170247_change_answer_to_boolean_in_responses.rb
@@ -1,6 +1,6 @@
 class ChangeAnswerToBooleanInResponses < ActiveRecord::Migration[7.1]
   def up
-    change_column :responses, :answer, :boolean, default: false
+    change_column :responses, :answer, :boolean
   end
 
   def down
