b9f927cdc965424955f44ba47a2d48edba4eb623
Cody Goeken <codygoeken@Codys-MacBook-Pro-2.local>
2024-04-26 09:30:41 -0700

    lint fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'surveys#index'
 
-  resources :surveys, only: [:index, :new, :create, :show] do
+  resources :surveys, only: %i[index new create show] do
     resources :responses, only: [:create]
   end
 end
--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class SurveysControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get surveys_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get surveys_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get surveys_create_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get surveys_show_url
     assert_response :success
   end
